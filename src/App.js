014d57ef9db83332ebea68037b02efb49a4548e3
rizky <rizkyjemal@gmail.com>
2024-06-25 17:19:18 +0700

    Cel

--- src/App.js
+++ src/App.js
@@ -56,7 +56,7 @@ function App() {
                         <div className="row no-gutters align-items-center">
                           <div className="col mr-2">
                             <div className="text-xs font-weight-bold text-primary text-uppercase mb-1">
-                              Proposal Requests
+                              Proposals
                             </div>
                             <div className="h5 mb-0 font-weight-bold text-gray-800">
                               {pengajuan.length}
