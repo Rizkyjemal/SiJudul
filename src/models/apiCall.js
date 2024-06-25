43d01449a8e428e1540fcc7e7d4d6600079ac94e
Seyditz <chordan345@gmail.com>
2024-06-25 17:23:18 +0700

    url

--- src/models/apiCall.js
+++ src/models/apiCall.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
-// const BASE_URL = "https://projectskripsi-fvwdncsc.b4a.run";
-const BASE_URL = "http://localhost:8000";
+const BASE_URL = "https://projectskripsi-fvwdncsc.b4a.run";
+// const BASE_URL = "http://localhost:8000";
 const TOKEN =
   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6ImNob3JkYW4zNDVAZ21haWwuY29tIiwidXNlcl9pZCI6MSwiZXhwIjoxNzE5MzgzMDg0LCJSb2xlcyI6WyJhZG1pbiIsImRvc2VuIiwibWFoYXNpc3dhIl19.-4_y-tLH8E1609q_ADI1C5fX4brH68gWgrAuuuEmLCk";
 
